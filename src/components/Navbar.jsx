ce2f861ccf4afc6417491acf313c94b6658c9cc2
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-04-27 23:52:26 +1000

    first

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar />
+        <Typography.Title level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Title>
+        {/* {<Button className="menu-control-container">
+
+        </Button>} */}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
